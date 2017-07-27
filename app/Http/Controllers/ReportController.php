54824f19c93d3acb50bc93f331a8b82bd819cd32
Bwana <kizomanizo@gmail.com>
2017-07-27 22:33:23 +0300

    Graphs and Stats for the reports module

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -23,12 +23,35 @@ class ReportController extends Controller
     {
     	$services = Service::all();
         $count = Patient::where('status', 1)->get();
-        $transactions = Patient_transaction::whereYear('created_at', date('Y'))->
-	        whereMonth('created_at', date('m'))->
-	        sum('type_price');
+        $transactions = Patient_transaction::whereYear('created_at', date('Y'))->whereMonth('created_at', date('m'))->sum('type_price');
         $payments = Patient_payment::whereYear('created_at', date('Y'))->
 	        whereMonth('created_at', date('m'))->
 	        sum('paid');
+        $days = Carbon::createFromDate(date('Y'), date('m'));
+        	$month = date('Y-m');
+			$start = Carbon::parse($month)->startOfMonth();
+			$end = Carbon::parse($month)->endOfMonth();
+
+			$dates = [];
+			while ($start->lte($end)) {
+			     $dates[] = $start->copy()->format('d');
+			     $start->addDay();
+			}
+					$year = date('Y');
+					$month = date('m');
+					# code...
+					$dailypatients = [];
+					foreach($dates as $date) {
+		$dailypatients[] = Patient::whereYear('created_at', $year)->
+			whereMonth('created_at', $month)->
+			whereDay('created_at', $date)->
+			count('uid');
+			}
+		$dt = Carbon::now();
+			if($dt->daysInMonth == 31) { $daysinamonth = implode(', ', (range(1, 31, 1))); 	}
+			else if($dt->daysInMonth == 30) { $daysinamonth = implode(', ', (range(1, 30, 1)));	}
+			else if($dt->daysInMonth == 29) { $daysinamonth = implode(', ', (range(1, 29, 1)));	}
+			else { $daysinamonth = implode(', ', (range(1, 28, 1))); }
         $statistics = array(
 	        'male' => Patient::where('sex', 'male')->
 	        	whereMonth('created_at', date('m'))->
@@ -37,6 +60,22 @@ class ReportController extends Controller
 	        'female' => Patient::where('sex', 'female')->
 	        	whereMonth('created_at', date('m'))->
 	        	distinct()->
+	        	count('uid'),
+	        'infants' => Patient::where('age', '<', 6)->
+		        whereMonth('created_at', date('m'))->
+		        distinct()->
+	        	count('uid'),
+	        'children' => Patient::where('age', '<', 12)->
+		        whereMonth('created_at', date('m'))->
+		        distinct()->
+	        	count('uid'),
+	        'adults' => Patient::where('age', '<', 65)->
+		        whereMonth('created_at', date('m'))->
+		        distinct()->
+	        	count('uid'),
+	        'elderly' => Patient::where('age', '>=', 65)->
+		        whereMonth('created_at', date('m'))->
+		        distinct()->
 	        	count('uid'),
 	        'revenue' => Patient_payment::whereYear('created_at', date('Y'))->
 	        	whereMonth('created_at', date('m'))->
@@ -72,50 +111,15 @@ class ReportController extends Controller
 	        	whereDay('created_at', date('d'))->
 	        	where('type', 'service')->
 	        	sum('type_price'),
-	        'month' => date('M'),
-	        'dailypatients' => DB::select("SELECT DATE_FORMAT(created_at, '%d') AS date, COUNT(patients.uid) AS patients FROM patients GROUP BY DAY(patients.created_at)"),
-	        // 'dailypatients' => DB::select("SELECT DATE_FORMAT(created_at, '%d') AS date FROM patients")
+	        'month' => date('F'),
+	        'dailypatients' => $dailypatients,
+	        'daysinamonth' => $daysinamonth
 	    );
-        $days = Carbon::createFromDate(date('Y'), date('m'));
-        	$month = date('Y-m');
-			$start = Carbon::parse($month)->startOfMonth();
-			$end = Carbon::parse($month)->endOfMonth();
 
-			$dates = [];
-			while ($start->lte($end)) {
-			     $dates[] = $start->copy()->format('d');
-			     $start->addDay();
-			}
-					$year = date('Y');
-					$month = date('m');
-					# code...
-					$dailypatients = [];
-					foreach($dates as $date) {
-					$dailypatients[] = Patient::whereYear('created_at', $year)->
-											whereMonth('created_at', $month)->
-											whereDay('created_at', $date)->
-											count('uid');
-						}
-
-				$dt = Carbon::now();
-					if($dt->daysInMonth == 31) {
-						$daysinamonth = "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31";
-						}
-					else if($dt->daysInMonth == 30) {
-						$dayinamonth = "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30";
-						}
-					else if($dt->daysInMonth == 29) {
-						$daysinamonth = "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29";
-						}
-					else {
-						$daysinamonth = "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28";
-						}
-			// return view('tests/index')->with('dailypatients', $dailypatients)->with('daysinamonth', $daysinamonth);
+			// print_r($daysinamonth);
         return view('reports/index')->
         	with('services', $services)->
         	with('count', $count)->
-        	with('statistics', $statistics)->
-        	with('dailypatients', $dailypatients)->
-        	with('daysinamonth', $daysinamonth);
+        	with('statistics', $statistics);
     }
 }
