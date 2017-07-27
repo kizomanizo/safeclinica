baecefa40befbe894e83b1ce854f6ba12dd74965
Bwana <kizomanizo@gmail.com>
2017-07-27 22:29:57 +0300

    Grouped JS files and used Chart.PieceLabel.js for Chart.js labels

--- public/js/custom/dailypatients.js
+++ public/js/custom/dailypatients.js
@@ -1,16 +1,12 @@
 var mon = '{{ $statistics['month'] }}';
-var serie = '{{ json_encode($dailypatients) }}';
+var serie = '{{ json_encode($statistics['dailypatients']) }}';
 var data = {
-        // A labels array that can contain any sort of values
-        labels: [
-            {{$daysinamonth}}
-            ],
-        // Our series array that contains series objects or in this case series data arrays
-        series: [
-            JSON.parse([serie])
-            ]
-};
-// Create a new line chart object where as first parameter we pass in a selector
-// that is resolving to our chart container element. The Second parameter
-// is the actual data object.
-new Chartist.Line('.ct-chart', data);
+        labels: [{{$statistics['daysinamonth']}}],
+        series: [JSON.parse([serie])],
+        };
+var options = {
+        low: -2,
+        showArea: false      
+        };
+;
+new Chartist.Line('.ct-chart', data, options);
